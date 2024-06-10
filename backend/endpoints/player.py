84916a36bf5fc4e988ab4f43e809ad79c228557b
Joe <joantpat@gmail.com>
2024-06-10 13:05:10 +0100

    Updated player endpoint

--- backend/endpoints/player.py
+++ backend/endpoints/player.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask import jsonify
-from models import Player
+from models import Player, MatchResult, RosterPlayerAssociation
 
 player_api = Blueprint("player", __name__)
 
@@ -20,13 +20,17 @@ def get_player(player_id):
         }
     }
     """
-    player = Player.query.filter(Player.player_id == player_id).first()
+    player = Player.query.filter(Player.steam_id == player_id).first()
     if not player:
         return jsonify({'success': False, 'data': {}})
     return jsonify({})
 
 @player_api.route("/<player_id>/matches")
 def get_player_matches(player_id):
-    player = Player.query.filteR(Player.player_id == player_id).first()
+    player = Player.query.filter(Player.steam_id == player_id).first()
     if not player:
         return jsonify({'success': False, 'data': [], 'error': f"Player with player_id {player_id} does not exist"})
+    # get rosters with player on
+    associations = RosterPlayerAssociation.query.filter(RosterPlayerAssociation.player_id == int(player_id))
+    matches = MatchResult.query.filter(MatchResult.team_id.in_([roster.roster_id for roster in associations])).all()
+    return jsonify({'success': True, 'data': [match.json() for match in matches]})
