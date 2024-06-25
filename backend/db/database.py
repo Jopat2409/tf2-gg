c1fc95570229528256a5fd4dd5dc864eb8066918
Joe <joantpat@gmail.com>
2024-06-25 17:43:04 +0100

    Updated db config

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,34 +3,50 @@ import sqlite3
 from flask import g, has_app_context
 from dotenv import load_dotenv
 
-from app import app
+from utils.logger import Logger
+from utils.file import read_required
 
 load_dotenv()
+database_logger = Logger.get_logger()
 
-DATABASE = f'{os.getenv("ENV", "dev")}.db'
+DATABASE = os.getenv("db", "dev.db")
+db_handle: sqlite3.Connection = None
+database_logger.log_info(f"Initialised database {DATABASE}")
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 def get_db():
     if has_app_context():
         db = getattr(g, '_database', None)
         if db is None:
             db = g._database = sqlite3.connect(DATABASE)
-            db.row_factory = sqlite3.Row
+            db.row_factory = dict_factory
         return db
     else:
-        return sqlite3.connect(DATABASE)
+        global db_handle
+        if db_handle is None:
+            db_handle = sqlite3.connect(DATABASE)
+            db_handle.row_factory = dict_factory
+        return db_handle
 
+# @database_logger.describe("Query Database: ")
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
-def insert_db(query, args=()):
+# @database_logger.describe("Update Database: ")
+def update_db(query, args=()):
     cur = get_db().execute(query, args)
+    get_db().commit()
     cur.close()
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+def regenerate_db():
+    database_logger.log_info(f"Regenerating database for {DATABASE}")
+    schema = read_required("db\\db.schema")
+    get_db().executescript(schema)
