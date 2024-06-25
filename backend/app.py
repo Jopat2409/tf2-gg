ce76618683c205694af6c482065e73cfbbead064
Joe <joantpat@gmail.com>
2024-06-25 17:42:47 +0100

    Updated database connection

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,25 @@
-from flask import Flask
-from database import db_session, init_db
+import sys
+
+from flask import Flask, g
 from endpoints.player import player_api
+from endpoints.team import team_api
+
+from utils.logger import Logger
+
+from db.database import regenerate_db
 
 app = Flask(__name__)
 app.register_blueprint(player_api, url_prefix='/player')
+app.register_blueprint(team_api, url_prefix='/team')
 
-init_db()
+Logger.init("logs", "flaskapp", True)
+
+args = sys.argv[1::]
+if "create_db" in args:
+    regenerate_db()
 
 @app.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
