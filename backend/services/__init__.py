1c0bc7b386306bcc7b9e8037f321c65aa745b6fe
Joe <joantpat@gmail.com>
2024-06-25 17:43:54 +0100

    Added PlayerService to * exports

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -1,4 +1,5 @@
 import services.matchservice as MatchService
 import services.teamservice as TeamService
+import services.playerservice as PlayerService
 
-__all__ = [MatchService, TeamService]
+__all__ = [MatchService, TeamService, PlayerService]
